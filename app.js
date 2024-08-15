188db25c882c945b8830c1c6e2a80334c7d55735
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-08-14 22:45:52 -0400

    added suggestion form data to be posted to api

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const climbingRouteRouter = require('./routes/climbingRouteRouter')
+const suggestionFormRouter = require('./routes/suggestionFormRouter')
 const cors = require('cors')
 const app = express();
 
 app.use(express.json());
 app.use(cors())
 app.use('/api/v1/climbing-routes/', climbingRouteRouter)
+app.use('/api/v1/form-data/', suggestionFormRouter)
 
 module.exports = app;
